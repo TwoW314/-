2d0bcd08b71d70d1e6edad2ce95da3db6e93cf9b
wyyyz <nmmlwc@gmail.com>
2023-11-06 11:22:33 +0800

    :test

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,8 +1,4 @@
 import * as nconf from "nconf"
 import {createClient, terminalClient} from "../common";
 
-terminalClient().then(client => {
-    console.log(client.userdata)
-}).catch(err => {
-    console.log(err)
-})
+console.log(Date.now())
